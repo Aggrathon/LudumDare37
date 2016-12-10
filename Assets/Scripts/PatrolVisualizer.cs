4d98812abfb19a711cb9765e70677ac266505a43
Aggrathon <antonbjo@gmail.com>
2016-12-10 13:43:31 +0200

    Create Robot

---
+++ Assets/Scripts/PatrolVisualizer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolVisualizer : MonoBehaviour {
+
+#if UNITY_EDITOR
+	public bool showPath;
+	void OnDrawGizmos()
+	{
+		if(showPath) {
+			int count = transform.childCount;
+			float step = 0.9f / (float)count;
+			for (int i = 0; i < count; i++)
+			{
+				Transform t1 = transform.GetChild(i);
+				Transform t2 = transform.GetChild((i + 1) % count);
+				Vector3 offset = t1.right * 0.05f;
+				Gizmos.color = Color.red;
+				Gizmos.DrawSphere(t1.position, 1f);
+				Gizmos.color = Color.red * (1f-i*step) + Color.white*(i*step);
+				Gizmos.DrawLine(t1.position, t2.position);
+				Gizmos.DrawLine(t1.position + offset, t2.position + offset);
+				Gizmos.DrawLine(t1.position - offset, t2.position - offset);
+			}
+		}
+	}
+#endif
+}
---
+++ Assets/Scripts/RobotAI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class RobotAI : MonoBehaviour {
+
+	public Transform patrol;
+	public float patrolDistance = 4f;
+	public bool patrolForward = true;
+	public int patrolFirstPoint = 0;
+
+	NavMeshAgent navAgent;
+	int patrolPosition;
+
+	void Start()
+	{
+		navAgent = GetComponent<NavMeshAgent>();
+		patrolPosition = patrolFirstPoint % patrol.childCount;
+		navAgent.SetDestination(patrol.GetChild(patrolPosition).position);
+	}
+
+	void Update()
+	{
+		if(Vector3.Distance(transform.position,patrol.GetChild(patrolPosition).position) < patrolDistance)
+		{
+			if(patrolForward)
+				patrolPosition = (patrolPosition + 1) % patrol.childCount;
+			else
+				patrolPosition = (patrolPosition + patrol.childCount - 1) % patrol.childCount;
+			navAgent.SetDestination(patrol.GetChild(patrolPosition).position);
+		}
+	}
+
+
+
+}
