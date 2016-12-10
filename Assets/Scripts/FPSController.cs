f2ae326f2453bab64f5ca4a0c13af80d48bfa444
Aggrathon <antonbjo@gmail.com>
2016-12-10 12:12:04 +0200

    Houses

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -6,17 +6,27 @@ using UnityEngine;
 public class FPSController : MonoBehaviour {
 
 	CharacterController cc;
-
-	// Use this for initialization
+	
 	void Start () {
 		cc = GetComponent<CharacterController>();
+		OnApplicationFocus(true);
 	}
 	
-	// Update is called once per frame
 	void Update ()
 	{
 		transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X"), 0);
 		cc.SimpleMove(transform.rotation*new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"))*3f);
 		Camera.main.transform.rotation *= Quaternion.Euler(-Input.GetAxis("Mouse Y"), 0, 0);
+	}
+
+	void OnApplicationFocus(bool hasFocus)
+	{
+		Cursor.lockState = hasFocus ? CursorLockMode.Locked : CursorLockMode.None;
+		Cursor.visible = hasFocus;
+	}
+
+	void OnDestroy()
+	{
+		OnApplicationFocus(false);
 	}
 }
